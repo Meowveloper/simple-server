15bb6291d31ea967558f7b821b730eb846095640
Meowveloper <meowveloper.dev@gmail.com>
2025-06-22 22:08:51 +0700

    static files and refactor

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
@@ -16,66 +17,134 @@ type Person struct {
 }
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to home page")
-	})
 
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello there!")
-	})
+	mux := http.NewServeMux()
 
-	http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
-		name := strings.TrimPrefix(r.URL.Path, "/greet/")
+	mux.HandleFunc("/", handle_root)
+	mux.HandleFunc("/hello", handle_hello)
+	mux.HandleFunc("/greet/", handle_greet)
+	mux.HandleFunc("/api/person", handle_get_person)
+	mux.HandleFunc("/api/register", handle_register)
+	mux.HandleFunc("/api/search", handle_search)
+	mux.HandleFunc("/api/form-submit", handle_form_submit)
 
-		if name == "" {
-			name = "Guest"
-		}
+	fs := http.FileServer(http.Dir("static"))
 
-		fmt.Fprintf(w, "Greetings %s!", name)
-	})
-
-	http.HandleFunc("/api/person", func(w http.ResponseWriter, r *http.Request) {
-		person := Person{
-			Name:       "Alice",
-			Age:        24,
-			Is_Student: true,
-		}
-		w.Header().Set("Content-Type", "application/json")
-
-		if err := json.NewEncoder(w).Encode(person); err != nil {
-			log.Printf("Error encoding json %v", err)
-			http.Error(w, "Internal server error", http.StatusInternalServerError)
-			return
-		}
-	})
-
-	http.HandleFunc("/api/register", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			http.Error(w, "Method now allowed", http.StatusMethodNotAllowed)
-			return
-		}
-		var new_person Person
-
-		if err := json.NewDecoder(r.Body).Decode(&new_person); err != nil {
-			log.Printf("Error decoding JSON Request body : %v", err)
-			http.Error(w, "Invalid request payload", http.StatusBadRequest)
-			return
-		}
-
-		if new_person.Name == "" || new_person.Age <= 0 {
-			http.Error(w, "Name and age are required fields", http.StatusBadRequest)
-			return
-		}
-
-		log.Printf("Received new person: Name = %s, Age = %d, City = %s, Is Student? = %t",
-			new_person.Name, new_person.Age, new_person.City, new_person.Is_Student,
-		)
-
-		w.WriteHeader(http.StatusCreated)
-
-		fmt.Fprintf(w, "Person %s was successfully created.\n", new_person.Name)
-	})
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	fmt.Println("Starting server on port 8000")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8000", mux))
+}
+
+func handle_root(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		http.ServeFile(w, r, "static/index.html")
+		return
+	}
+}
+
+func handle_hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello there!")
+}
+
+func handle_greet(w http.ResponseWriter, r *http.Request) {
+	name := strings.TrimPrefix(r.URL.Path, "/greet/")
+
+	if name == "" {
+		name = "Guest"
+	}
+
+	fmt.Fprintf(w, "Greetings %s!", name)
+}
+
+func handle_get_person(w http.ResponseWriter, r *http.Request) {
+	person := Person{
+		Name:       "Alice",
+		Age:        24,
+		Is_Student: true,
+	}
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(person); err != nil {
+		log.Printf("Error encoding json %v", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func handle_register(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method now allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	var new_person Person
+
+	if err := json.NewDecoder(r.Body).Decode(&new_person); err != nil {
+		log.Printf("Error decoding JSON Request body : %v", err)
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	if new_person.Name == "" || new_person.Age <= 0 {
+		http.Error(w, "Name and age are required fields", http.StatusBadRequest)
+		return
+	}
+
+	log.Printf("Received new person: Name = %s, Age = %d, City = %s, Is Student? = %t",
+		new_person.Name, new_person.Age, new_person.City, new_person.Is_Student,
+	)
+
+	w.WriteHeader(http.StatusCreated)
+
+	fmt.Fprintf(w, "Person %s was successfully created.\n", new_person.Name)
+}
+
+func handle_search(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query().Get("query")
+	limit := r.URL.Query().Get("limit")
+
+	if query == "" {
+		http.Error(w, "Missing query string", http.StatusBadRequest)
+		return
+	}
+
+	limit_value := 0
+	if limit != "" {
+		var err error
+		limit_value, err = strconv.Atoi(limit)
+		if err != nil {
+			http.Error(w, "Invalid 'limit' parameter", http.StatusBadRequest)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "searching for: '%s' with a limit of %d results", query, limit_value)
+}
+
+func handle_form_submit(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "request method not allowed", http.StatusBadRequest)
+		return
+	}
+
+	if err := r.ParseForm(); err != nil {
+		log.Printf("Error parsing form data %v", err)
+		http.Error(w, "error parsing form data", http.StatusBadRequest)
+		return
+	}
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	email := r.FormValue("email")
+
+	if username == "" || password == "" {
+		http.Error(w, "username and password are required", http.StatusBadRequest)
+		return
+	}
+
+	log.Printf("received form submission: username=%s, email=%s.", username, email)
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "form submitted successfully! welcome, %s", username)
 }
